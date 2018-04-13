5a9410186f15a26a1ebebe08abb0b4de9d327068
Greg Jose dos Santos <g.santos@dom_qp.com.br>
2018-04-13 08:25:51 -0300

    Cadastro de Caminhão

--- src/app/caminhao/caminhao.component.ts
+++ src/app/caminhao/caminhao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-caminhao',
@@ -10,9 +11,26 @@ export class CaminhaoComponent implements OnInit {
   public maskPlaca = [/[A-z]/, /[A-z]/, /[A-z]/,'-', /\d/, /\d/, /\d/, /\d/];
   public maskAno = [/\d/, /\d/, /\d/, /\d/];
 
+  public formulario: FormGroup = new FormGroup({
+    'marca': new FormControl("", [Validators.required]),
+    'modelo': new FormControl(null, [Validators.required]),
+    'placa': new FormControl(null,[Validators.required, Validators.minLength(8)]),
+    'ano': new FormControl(null, [Validators.required, Validators.minLength(4)]),
+    'apelido': new FormControl(null, [Validators.required]),
+  })
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
+
+  public salvarCaminhao() : void {
+
+    console.log(this.formulario.value);
+  }
+  public limparFormulario(): void{
+    this.formulario.reset();
+  }
 }
