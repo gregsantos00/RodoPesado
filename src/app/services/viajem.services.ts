79937cac173fbf72194c50f1d45d79e00f9522b9
Greg Jose dos Santos <g.santos@dom_qp.com.br>
2018-05-03 08:02:14 -0300

    Cadastro de viagens

---
+++ src/app/services/viajem.services.ts
@@ -0,0 +1,11 @@
+import { Observable } from "rxjs/Observable";
+import * as fb from 'firebase';
+
+export class ViajemService {
+
+// public getMotoristas(termo: string): Observable<string>{
+
+//     fb.database().ref('viajem').o
+// }
+
+}
--- src/app/viajem/viajem.component.ts
+++ src/app/viajem/viajem.component.ts
@@ -1,25 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
+import '../shared/rxjs-extensions'
+import { Caminhao } from '../model/cominhao.model';
+import { CaminhaoService } from '../services/caminhao.service';
 
 @Component({
   selector: 'app-viajem',
   templateUrl: './viajem.component.html',
-  styleUrls: ['./viajem.component.css']
+  styleUrls: ['./viajem.component.css'],
+  providers: [CaminhaoService]
 })
 export class ViajemComponent implements OnInit {
 
   public formulario: FormGroup = new FormGroup({
-    'key': new FormControl(null),
-    'marca': new FormControl("", [Validators.required]),
-    'modelo': new FormControl(null, [Validators.required]),
-    'placa': new FormControl(null, [Validators.required, Validators.minLength(8)]),
-    'ano': new FormControl(null, [Validators.required, Validators.minLength(4)]),
-    'apelido': new FormControl(null, [Validators.required]),
+    'caminhao': new FormControl("", [Validators.required])
+    // 'modelo': new FormControl(null, [Validators.required]),
+    // 'placa': new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    // 'ano': new FormControl(null, [Validators.required, Validators.minLength(4)]),
+    // 'apelido': new FormControl(null, [Validators.required]),
   })
-  
-  constructor() { }
+  private subs: Subject<string> = new Subject<string>()
+  public motoristas: Observable<string[]>
+  public caminhoes: Caminhao[] = []
+
+  constructor(private caminhaoService: CaminhaoService) { }
 
   ngOnInit() {
+
+    this.caminhaoService.ListarCaminhao()
+      .then((x: Caminhao[]) => {
+        this.caminhoes = x;
+      })
+    // this.motoristas = this.subs
+    //   .debounceTime(1000)
+    //   .distinctUntilChanged()
+    //   .switchMap((valor: string) => {
+    //     if (valor.trim() === '') {
+    //       return Observable.of<string[]>([]);
+    //     }
+    //     console.log('Requisição')
+    //     return this.servico.pesquisarOferta(valor)
+    //   })
+    //   .catch((error: any) => {
+    //     console.log(error)
+    //     return Observable.of<string[]>([]);
+    //   })
   }
 
   public salvarViajem(): void {
