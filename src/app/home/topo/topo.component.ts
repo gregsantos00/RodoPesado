974a18ff0ab0d161c3179be35100a9c296577ed1
Greg Jose dos Santos <g.santos@dom_qp.com.br>
2018-06-13 15:08:36 -0300

    20180613

--- src/app/home/topo/topo.component.ts
+++ src/app/home/topo/topo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as fb from 'firebase';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-topo',
@@ -10,13 +11,18 @@ export class TopoComponent implements OnInit {
 
   public logado : boolean = false;
 
-  constructor() { }
+  constructor(private auth : AuthService) { }
 
   ngOnInit() {
     fb.auth().onAuthStateChanged((user: any) => {
       console.log(user.email);
       this.logado =  user.email != undefined && user.email != ''
     })
+  }
+
+  public LogOut(): void {
+    this.logado = false;
+    this.auth.logout();
   }
 
 }
--- src/app/viajem/viajem.component.ts
+++ src/app/viajem/viajem.component.ts
@@ -16,8 +16,8 @@ import { CaminhaoService } from '../services/caminhao.service';
 export class ViajemComponent implements OnInit {
 
   public formulario: FormGroup = new FormGroup({
-    'caminhao': new FormControl("", [Validators.required])
-    // 'modelo': new FormControl(null, [Validators.required]),
+    'caminhao': new FormControl("", [Validators.required]),
+    'motorista': new FormControl("", [Validators.required])
     // 'placa': new FormControl(null, [Validators.required, Validators.minLength(8)]),
     // 'ano': new FormControl(null, [Validators.required, Validators.minLength(4)]),
     // 'apelido': new FormControl(null, [Validators.required]),
